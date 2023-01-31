7fe786710b0e53cf3182a8db80574487d252c5b3
bill <cleber.flauzino@hotmail.com>
2023-01-30 22:30:15 -0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+//programa que modifica o valor dos atributos
+public class Main {
+  int x = 10;
+
+  public static void main(String[] args) {
+    Main myObj = new Main();
+    myObj.x = 25;
+    System.out.println(myObj.x);
+  }
+}
